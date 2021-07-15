97f52ade9ba059de6e27d139e6eb64d71a27b204
NenadZdravkovic97 <nenadzdravkovic97@yahoo.com>
2021-07-15 11:50:48 +0200

    Decdided to switch to one-page design and I added sticky nav and gallery with overlay

--- js/main.js
+++ js/main.js
@@ -12,3 +12,21 @@ menuBtn.addEventListener('click', (e) => {
         navItems.classList.add('active');
     }
 });
+
+
+
+// Sticky nav
+window.addEventListener('scroll', (e) => {
+    let main = document.querySelector('.main-nav');
+    let landingPage = document.querySelector('.video-container');
+    let landingHeight = landingPage.offsetHeight;
+    let scrollPos = window.scrollY;
+
+    window.addEventListener('scroll', () => {
+        if (scrollPos > landingHeight) {
+            main.classList.add('sticky');
+        } else {
+            main.classList.remove('sticky');
+        }
+    })
+})
